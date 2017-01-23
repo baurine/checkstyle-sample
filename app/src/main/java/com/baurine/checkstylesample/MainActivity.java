d311e8fe65e3ea33c645f4a707a759b0c7af3dc7
baurine <2008.hbl@gmail.com>
2017-01-23 14:55:45 +0800

    add a sample method to test checkstyle whether works normal

--- app/src/main/java/com/baurine/checkstylesample/MainActivity.java
+++ app/src/main/java/com/baurine/checkstylesample/MainActivity.java
@@ -10,4 +10,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private void TestCheckStyle()
+    throws RuntimeException
+    {
+        boolean TestBoolean = 5 > 6 &&
+                4>5;
+        int ret = 5 / 0;
+    }
 }
